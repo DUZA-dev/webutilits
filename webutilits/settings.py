f15b6e9b75ca93d5c1e32118fabfb7c4f0ba1a43
s_s <self.script.super@gmail.com>
2020-04-13 21:59:57 +0300

    Версия для гихаба

--- webutilits/settings.py
+++ webutilits/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a+$%7qy_26*4ocbt@=f4ag7u&3gh7a*t0evl4mv=#q-0zc@hew'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
